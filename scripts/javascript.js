9d6f410a70ea4bbfa9c2b40de020df26725db2c0
Nicholas <nicholasbarizo@gmail.com>
2020-12-24 13:36:33 -0800

    turned individual number button eventlisteners into a single body event listener and added possible mobile delay fix

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,46 +1,52 @@
 const displayNumber = document.getElementById('displayNumber');
 const numberButtons = document.querySelectorAll('button.numberButton');
 const operatorButtons  = document.querySelectorAll('button.operatorButton');
-
-numberButtons.forEach(numberButtonSelected => clickNumberButton(numberButtonSelected));
-operatorButtons.forEach(operatorButtonSelected => clickOperatorbutton(operatorButtonSelected));
-let canDecimal = true;
 let num1 = 0;
 let num2 = 0;
-// Array.prototype.forEach.call(numberButton, function(numberButtonSelected) {
-
+let canDecimal = true;
+document.body.addEventListener('click', function (event) {
+    if(event.target.classList.contains('numberButton')) {
+        clickNumberButton(event.target);
+    }
+    else if(event.target.classList.contains('operatorButton')) {
+        clickOperatorButton(event.target);
+    }
+});
+document.body.addEventListener('touchstart', function(event) {
+    if(event.target.classList.contains('numberButton')) {
+        clickNumberButton(event.target);
+    }
+    else if(event.target.classList.contains('operatorbutton')) {
+        clickOperatorButton(event.target);
+    }
+});
 
 let operatorPressed = false;
 let operator;
 
-// Array.prototype.forEach.call(operatorButton, function(operatorButtonSelected) {
-function clickOperatorbutton(operatorButtonSelected) {
-    operatorButtonSelected.addEventListener('click', () => {
-        if(operatorPressed == false){
-            num2 = num1;
-            num1 = displayNumber.textContent;
-            // console.log(num1 + 'num1');
-            // console.log(num2 + 'num2');
+function clickOperatorButton(operatorButtonSelected) {
+    if(operatorPressed == false){ //Turn inputs into
+        num2 = num1;
+        num1 = displayNumber.textContent;
+    }
+    if(operatorButtonSelected.id != 'equalsButton'){
+        if(operatorPressed == true){
+            operatorButtons.forEach(operatorButtonSelected => 
+                operatorButtonSelected.style.setProperty('background-color', '#1dd3b0')
+            );
         }
-        if(operatorButtonSelected.id != 'equalsButton'){
-            if(operatorPressed == true){
-                operatorButtons.forEach(operatorButtonSelected => 
-                    operatorButtonSelected.style.setProperty('background-color', '#1dd3b0')
-                );
-            }
-            operatorButtonSelected.style.setProperty('background-color', '#F7EF81');
-            operator = operatorButtonSelected.id;
-            operatorPressed = true;
-            displayNumber.textContent = '0';
+        operatorButtonSelected.style.setProperty('background-color', '#F7EF81');
+        operator = operatorButtonSelected.id;
+        operatorPressed = true;
+        displayNumber.textContent = '0';
 
 
-            console.log(num2);
-        }
-        else{
-            num2 = calculateAnswer(operator, num1, num2);
-            displayNumber.textContent = num2.toString().slice(0, 14);
-        }
-    });
+        console.log(num2);
+    }
+    else{
+        num2 = calculateAnswer(operator, num1, num2);
+        displayNumber.textContent = num2.toString().slice(0, 14);
+    }
 }
 
 function calculateAnswer(operator, num1, num2) {
@@ -65,7 +71,8 @@ function calculateAnswer(operator, num1, num2) {
 }
 
 function clickNumberButton(numberButtonSelected) {
-    numberButtonSelected.addEventListener('click', () => {
+    // console.log(numberButtonSelected);
+    // numberButtonSelected.addEventListener('click', () => {
         if(operatorPressed == true){
             operatorButtons.forEach(operatorButtonSelected =>
                 operatorButtonSelected.style.setProperty('background-color', '#1dd3b0')
@@ -80,7 +87,6 @@ function clickNumberButton(numberButtonSelected) {
         }
         else if(numberButtonSelected.textContent == 'CLEAR'){
             displayNumber.textContent = '0';
-            console.log('hi');
             canDecimal = true;
             operatorButtons.forEach(operatorButtonSelected =>
                 operatorButtonSelected.style.setProperty('background-color', '#1dd3b0')
@@ -88,7 +94,7 @@ function clickNumberButton(numberButtonSelected) {
             num1 = 0;
             num2 = 0;
         }
-        else if(numberButtonSelected.textContent == '   '){
+        else if(numberButtonSelected.textContent == '-/+'){
             console.log('sdf');
             displayNumber.textContent *= -1;
         }
@@ -105,7 +111,7 @@ function clickNumberButton(numberButtonSelected) {
         else{
             displayNumber.textContent += numberButtonSelected.innerHTML;
         }
-    });
+    // });
 }
 
 
